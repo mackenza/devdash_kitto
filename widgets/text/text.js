1840343534e39cdf81b7dba3b333dff024ac502b
mackenza <a.mackenzie@gmail.com>
2017-01-14 10:29:04 -0500

    wip adding optional updated-at to widgets

--- widgets/text/text.js
+++ widgets/text/text.js
@@ -8,7 +8,13 @@ Widget.mount(class Text extends Widget {
   status() {
     if (!this.state.status) { return ""; }    
     
-    return`status-${this.state.status}`;
+    return `status-${this.state.status}`;
+  }
+
+  showUpdated() {
+    if (!this.props.showupdated) { return ""; }
+
+    return updatedAt(this.state.updated_at);
   }
   
   render() {
@@ -17,7 +23,7 @@ Widget.mount(class Text extends Widget {
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
-        <p className="updated-at">{updatedAt(this.state.updated_at)}</p>
+        <p className="updated-at">{this.showUpdated()}</p>
       </div>
     );
   }
